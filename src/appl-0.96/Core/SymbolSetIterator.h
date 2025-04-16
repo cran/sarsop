7487a37187b05efa214327e5365da4ffcd0837f9
Carl Boettiger <cboettig@gmail.com>
2025-04-16 04:50:08 +0000

    version 0.6.16

--- src/appl-0.96/Core/SymbolSetIterator.h
+++ src/appl-0.96/Core/SymbolSetIterator.h
@@ -1,4 +1,3 @@
-// code edit assisted by ChatGPT 2024-06-24
 #ifndef SymbolSetIterator_H
 #define SymbolSetIterator_H
 
@@ -7,66 +6,82 @@
 #include "Const.h"
 using namespace std;
 //using namespace momdp;
-
-namespace momdp
+namespace momdp 
 {
 template <typename T> class SymbolSet;
 
 template <typename T>
 class SymbolSetIterator
 {
-  SymbolSet<T> *parentSet;
-  int symbolID;
 public:
-  using iterator_category = input_iterator_tag;
-  using value_type = T;
-  using difference_type = ptrdiff_t;
-  using pointer = T*;
-  using reference = T&;
+	using iterator_category = std::input_iterator_tag;
+	using value_type = T;
+	SymbolSet<T> *parentSet;
+	int symbolID;
 
-  SymbolSetIterator(SymbolSet<T> *_parentSet, int _symbolID)
-    : parentSet(_parentSet), symbolID(_symbolID) {}
 
-  SymbolSetIterator(const SymbolSetIterator& mit)
-    : parentSet(mit.parentSet), symbolID(mit.symbolID) {}
+public:
+	SymbolSetIterator(SymbolSet<T> *_parentSet, int _symbolID) 
+	{
+		parentSet = _parentSet;
+		symbolID = _symbolID;
+	}
 
-  SymbolSetIterator<T>& operator++()
-  {
-    ++symbolID;
-    return *this;
-  }
+	SymbolSetIterator(const SymbolSetIterator& mit) 
+	{
+		this->parentSet = mit.parentSet;
+		this->symbolID = mit.symbolID;
+	}
 
-  SymbolSetIterator<T> operator++(int)
-  {
-    SymbolSetIterator<T> tmp(*this);
-    ++symbolID;
-    return tmp;
-  }
+	SymbolSetIterator<T>& operator++() 
+	{
+		++symbolID;
+		return *this;
+	}
 
-  bool operator==(const SymbolSetIterator& rhs) const
-  {
-    return parentSet == rhs.parentSet && symbolID == rhs.symbolID;
-  }
+	void operator++(int) 
+	{
+		symbolID++;
+	}
 
-  bool operator!=(const SymbolSetIterator& rhs) const
-  {
-    return !(*this == rhs);
-  }
+	bool operator==(const SymbolSetIterator& rhs) 
+	{
+		if(parentSet == rhs.parentSet && symbolID == rhs.symbolID)
+		{
+			return true;
+		}
+		return false;
+	}
 
-  int operator*() const
-  {
-    return symbolID;
-  }
+	bool operator!=(const SymbolSetIterator& rhs) 
+	{
+		if(parentSet != rhs.parentSet || symbolID != rhs.symbolID)
+		{
+			return true;
+		}
+		return false;
+	}
 
-  T& value() const
-  {
-    return parentSet->getSymbol(symbolID);
-  }
+	int operator*() 
+	{
+		return symbolID;
+	}
 
-  int index() const
-  {
-    return symbolID;
-  }
+	T& value() 
+	{
+		return parentSet->getSymbol(symbolID);
+	}
+	int index() 
+	{
+		return symbolID;
+	}
+
 };
+
 }
+
+
 #endif
+
+
+
